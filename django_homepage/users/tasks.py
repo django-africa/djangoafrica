097e7e1c79ab091b1f074a012e166993456695f8
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-03-14 10:33:50 +0100

    celery has no task()

--- django_homepage/users/tasks.py
+++ django_homepage/users/tasks.py
@@ -5,7 +5,7 @@ from config import celery_app
 User = get_user_model()
 
 
-@celery_app.task()
+#@celery_app.task()
 def get_users_count():
     """A pointless Celery task to demonstrate usage."""
     return User.objects.count()
