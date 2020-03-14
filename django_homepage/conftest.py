e604631b92725e89a0fd79737ede49cca5e0ae24
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-03-14 14:22:05 +0100

    Update conftest.py

--- django_homepage/conftest.py
+++ django_homepage/conftest.py
@@ -1,20 +1,20 @@
-"""import pytest
-from django.test import RequestFactory
+#import pytest
+#from django.test import RequestFactory
 
-from django_homepage.users.models import User
-from django_homepage.users.tests.factories import UserFactory
-
-
-@pytest.fixture(autouse=True)
-def media_storage(settings, tmpdir):
-    settings.MEDIA_ROOT = tmpdir.strpath
+#from django_homepage.users.models import User
+#from django_homepage.users.tests.factories import UserFactory
 
 
-@pytest.fixture
-def user() -> User:
-    return UserFactory()
+#@pytest.fixture(autouse=True)
+#def media_storage(settings, tmpdir):
+    #settings.MEDIA_ROOT = tmpdir.strpath
 
 
-@pytest.fixture
-def request_factory() -> RequestFactory:
-    return RequestFactory()
+#@pytest.fixture
+#def user() -> User:
+   # return UserFactory()
+
+
+#@pytest.fixture
+#def request_factory() -> RequestFactory:
+    #return RequestFactory()
