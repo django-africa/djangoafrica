ea0312550278399f0ab93584b25ae706275ff0b7
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-03-27 13:11:06 +0100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+# SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["djangoafrica.com"])
 
