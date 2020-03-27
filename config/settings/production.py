6ad7477c9429887b16c5aab5715049a5c8c49472
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2020-03-27 15:45:47 +0100

    commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,12 +19,12 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 
-ALLOWED_HOSTS = ["*","djangoafrica.com"]
+ALLOWED_HOSTS = ["*", "djangoafrica.com"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
 
-DATABASE_URL = ['postgres://vvusfbstdseymi:02ee2ccb6ee4213b33e3cf3e958da78c70036c5b1426066754a31faeb4e92ed3@ec2-34-200-101-236.compute-1.amazonaws.com:5432/db9fu9ast3jhp3']
+DATABASE_URL = 'postgres://vvusfbstdseymi:02ee2ccb6ee4213b33e3cf3e958da78c70036c5b1426066754a31faeb4e92ed3@ec2-34-200-101-236.compute-1.amazonaws.com:5432/db9fu9ast3jhp3'
 
 DATABASES = {
     'default': {
