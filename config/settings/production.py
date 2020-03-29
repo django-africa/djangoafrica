428c12c23ad4ab20c20fa9359c3b80bf32c06b64
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-03-29 08:51:33 +0100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,6 @@ from .base import *  # noqa
 from .base import env
 
 # GENERAL
-DEBUG = env.bool("DJANGO_DEBUG", False)
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
