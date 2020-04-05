8eaf3eb99ff5277f8603d8981f3116f09cb03b3a
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-04-05 20:43:37 +0100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -21,7 +21,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 
-ALLOWED_HOSTS = ["*", "djangoafrica.com"]
+ALLOWED_HOSTS = ["http://forum.djangoafrica.com", "http://djangoafrica.com"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
