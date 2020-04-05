ce7bb7937445ff50106ff337d4cf15a4b0484b82
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-04-05 23:18:28 +0100

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -299,7 +299,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "username"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "none"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "django_homepage.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
