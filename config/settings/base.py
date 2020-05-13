e0a39be3cce7d01dcd4f631eea9f1e18a7942cc6
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-05-13 09:26:51 +0100

    change time zone to Africa/Lagos

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ DEBUG = env.bool("DJANGO_DEBUG", True)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC"
+TIME_ZONE = "Africa/Lagos"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
